de9b224056e3b0e75496fa2f2bb34f2f53963f4b
Leo <156511059+Gol-D-Leo@users.noreply.github.com>
2024-11-15 05:20:39 +0200

    matrix.js

--- matrix.js
+++ matrix.js
@@ -12,6 +12,20 @@ canvas.height = canvasHeight;
 const characters = [
   "0",
   "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
 ];
 
 // Create an array of columns
